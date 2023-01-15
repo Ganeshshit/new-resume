97a6b2f7df14fb48dd0de7060afd02ecd6bd8495
Coderganesh <108095949+Ganeshshit@users.noreply.github.com>
2023-01-15 08:36:50 +0530

    ....

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
 function lightFunction() {
-    var elment = document.body;
-    let about=document.getElementById('about');
-    let project=document.getElementById('project');
-    // let project2=document.getElementsByClassName('card-container');
-    project.classList.toggle("card-dark-mood");
-    // project2.classList.toggle("card-dark-mood");
-    // setInterval(() => {
-      
-      // }, 1000);
-      setTimeout(() => {
-        // audio=new Audio('lamborgini.mp3');
-        audio.play();
-      stop()
-    }, 1000);
+  var elment = document.body;
+  let about = document.getElementById('about');
+  let footer = document.getElementById("footer")
 
-    about.classList.toggle("about-mood");
-    elment.classList.toggle("green-mood");
+  project.classList.toggle("about-mood");
+
+  // project.classList
+  setTimeout(() => {
+    audio = new Audio('lamborgini.mp3');
+    // audio.play();
+    stop()
+  }, 100);
+
+  about.classList.toggle("about-mood");
+  elment.classList.toggle("green-mood");
+  footer.classList.toggle("footer-dark")
 }
-// addEventListener scroll
-// $(function () {
-//     $("#nav-placeholder").load("nav.html");
-// });
-let logo=document.getElementsByClassName('logo');
+
+let logo = document.getElementsByClassName('logo');
 
 let mybutton = document.getElementById('scroll-btn');
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
-  function topFunction() {
+}
+function topFunction() {
 
-    // document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
+  // document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; 
+}
